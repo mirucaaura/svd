6ad15cfadfa45827eb9250875a3aa4811dafbdb8
mirucaaura <yasunari.hikima@gmail.com>
2022-07-04 13:12:28 +0900

    chenge the way of generating a random matrix

--- examples/sample.py
+++ examples/sample.py
@@ -7,8 +7,8 @@ m, n, p = 5, 4, 3
 mu = np.arange(p, 0, -1)
 N = np.diag(mu)
 
-Ur, _ = np.linalg.qr(np.random.rand(m, n))
-Vr, _ = np.linalg.qr(np.random.rand(n, n))
+Ur, _ = np.linalg.qr(np.random.normal(size=(m, n)))
+Vr, _ = np.linalg.qr(np.random.normal(size=(n, n)))
 S = np.diag(np.arange(n, 0, -1))
 A = Ur @ S @ Vr.T
 
